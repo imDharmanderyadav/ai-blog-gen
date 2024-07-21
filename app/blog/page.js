4c8f6d3133c8934577117a05b04ef51f9090725c
Dharmander Yadav <dharmander832@gmail.com>
2024-07-22 05:09:00 +0530

    add the api page and make it runable

--- app/blog/page.js
+++ app/blog/page.js
@@ -1,19 +1,39 @@
-import React from 'react'
+'use client'
+import { useSearchParams } from 'next/navigation';
+import React, { useEffect, useState } from 'react';
+import Header from '@/components/Header';
 
-const Blog = ({ title, content }) => {
+const Blog = () => {
+  const [blog, setBlog] = useState({ title: "Generating the Blog....", content: "" });
+  const params = useSearchParams();
+  const prompt = params.get("prompt");
+
+  useEffect(() => {
+    const fetchBlog = async () => {
+      try {
+        // console.log("params: " + prompt);
+        let response = await fetch("/api/generate?prompt=" + prompt);
+        let data = await response.json();
+        setBlog(JSON.parse(data.json));
+        // return;
+      } catch (error) {
+        setBlog({title:"",content:"<h1>Error fetching blog....</h1> <br/>Please try again...."})
+        console.error("Error fetching blog:", error);
+      }
+    };
+
+    fetchBlog();
+  }, [prompt]);
+
   return (
-   <>
-   // components/Blog.js
-   <div className="max-w-3xl mx-auto py-8 px-4">
-      <h1 className="text-3xl font-bold mb-4">{title}</h1>
-      <div className="prose prose-lg" dangerouslySetInnerHTML={{ __html: content }}>{content}</div>
+<>
+<Header/>
+    <div className="w-10/12 space-y-12 mx-auto py-8 px-4">
+      <h1 className="text-3xl font-bold mb-4">{blog.title!="Generating the Blog...."?"":blog.title}</h1>
+      <div className="prose prose-lg" dangerouslySetInnerHTML={{ __html: blog.content }}></div>
     </div>
-
+    </>
+  );
+};
 
 export default Blog;
-
-   </>
-  )
-}
-
-export default Blog
